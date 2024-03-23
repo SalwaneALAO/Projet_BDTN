a3051471b191260718981a6f08adedfcb5b7be6e
Emmanuel AGBAHOLOU <manuagbaholou@gmail.com>
2024-03-23 18:19:37 +0100

    modifiation class create consoles and manufacturer

--- API_Sales/obj/Debug/net6.0/API_Sales.AssemblyInfo.cs
+++ API_Sales/obj/Debug/net6.0/API_Sales.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Sales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2650b62c5cfc912992f2f0e4a8fdd56ff56597b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Sales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Sales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Client_Sales/obj/Debug/net6.0/Client_Sales.AssemblyInfo.cs
+++ Client_Sales/obj/Debug/net6.0/Client_Sales.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client_Sales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2650b62c5cfc912992f2f0e4a8fdd56ff56597b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client_Sales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client_Sales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
